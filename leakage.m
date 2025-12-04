c5a3ab0cf28d4c07c4ec216d273cc7fe85cb9adf
swangneu <wang.shum@northeastern.edu>
2025-12-03 19:12:58 -0500

    v5

--- leakage.m
+++ leakage.m
@@ -1,11 +1,15 @@
 clear;
 clc;
-L11 = 8.56725e-5;
-L21 = 3.21438e-5;
-L31 = 3.17159e-5;
-L22 = 0.000291667;
-L32 = 0.000154833;
-L33 = 0.000287074;
+
+f = 1e5;
+omega = 2*pi*f;
+
+L11 = 0.000320008;
+L21 = 296.22/ omega;
+L31 = 296.22/ omega;
+L22 = 0.00116676;
+L32 = 471.983 / omega;
+L33 = 0.00116676;
 
 L = [L11 L21 L31;
     L21 L22 L32;
@@ -13,6 +17,7 @@ L = [L11 L21 L31;
 
 %%
 figure;
+L = L*1e6; % uH
 h = heatmap(L);
 h.Title = 'Inductance Matrix L';
 h.XLabel = 'Current index j';
@@ -20,3 +25,11 @@ h.YLabel = 'Flux linkage index i';
 h.CellLabelFormat = '%.3f';
 colormap(parula);   % or 'jet', 'turbo', 'hot'
 colorbar;
+
+%% cantilever
+Lm = L11;
+n2 = L21/L11;
+n3 = L31/L11;
+L1 = L32*L11^2/L21/L31-L11;
+L2 = L22*L11^2/L21^2-L32*L11^2/L21/L31;
+L3 = L33*L11^2/L31^2-L32*L11^2/L21/L31;
